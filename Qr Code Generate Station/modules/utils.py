d926c44b38713725e56442b0067d97de5ded0d3a
nemoirisgurl <noppanart5777@gmail.com>
2025-10-31 05:35:51 +0700

    qr-reader

--- Qr Code Generate Station/modules/utils.py
+++
@@ -1,8 +0,0 @@
-# modules/utils.py
-import os, time
-
-def ensure_dir(path):
-    os.makedirs(path, exist_ok=True)
-
-def timestamp_name(prefix="file", ext="jpg"):
-    return f"{prefix}_{time.strftime('%Y%m%d_%H%M%S')}.{ext}"
